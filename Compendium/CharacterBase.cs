385e865ba8db892c739c0d97e7d034276cb75793
Scott Falbo <scottfalboart@gmail.com>
2025-01-15 12:37:25 -0800

    dropping directory structure

---
+++ Compendium/CharacterBase.cs
@@ -0,0 +1,10 @@
+﻿// ------------------------------------------
+// Game Engine: Mechanics and Collections
+// ------------------------------------------
+
+namespace Compendium;
+
+public abstract class CharacterBase(string name)
+{
+    public string Name { get; private set; } = name;
+}
---
+++ Compendium/ItemBase.cs
@@ -0,0 +1,10 @@
+﻿// ------------------------------------------
+// Game Engine: Mechanics and Collections
+// ------------------------------------------
+
+namespace Compendium;
+
+public abstract class ItemBase(string name)
+{
+    public string Name { get; private set; } = name;
+}
