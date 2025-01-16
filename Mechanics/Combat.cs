b8de0e54c66025b6ad19f014a8f7842247007f6e
Scott Falbo <scottfalboart@gmail.com>
2025-01-15 16:57:25 -0800

    adding mechanic classes

---
+++ Mechanics/Combat.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------
+// Game Engine: Mechanics and Collections
+// ------------------------------------------
+
+using Compendium;
+
+namespace Mechanics;
+
+public class Combat(Player player, List<Creature> creatures)
+{
+    private readonly List<Creature> _creatures = creatures;
+
+    private readonly Dice _dice = new();
+
+    private readonly Player _player = player;
+}
---
+++ Mechanics/Dice.cs
@@ -0,0 +1,22 @@
+﻿// ------------------------------------------
+// Game Engine: Mechanics and Collections
+// ------------------------------------------
+
+namespace Mechanics;
+
+public class Dice
+{
+    public static int Roll(int sides, int count = 1)
+    {
+        var total = 0;
+
+        for (var i = 0; i < count; i++)
+        {
+            total += Roll(sides);
+        }
+
+        return total;
+    }
+
+    private static int Roll(int sides) => new Random().Next(1, sides + 1);
+}
