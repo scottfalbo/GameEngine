0d4d98be67037f5c87cb87584dd84ae381748381
Scott Falbo <scottfalboart@gmail.com>
2025-01-27 17:33:49 -0800

    implementing interfaces

---
+++ GameEngine/Interfaces/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿// ------------------------------------------
+// Game Engine: Mechanics and Collections
+// ------------------------------------------
+
+namespace GameEngine.Interfaces;
+
+public interface IPlayer
+{
+    public int Currency { get; }
+
+    public int Experience { get; }
+
+    public int HitPoints { get; }
+}
